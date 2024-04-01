2099f1874e6af191689b023a422132756115a6c9
javashashavali <javashashavali@gmail.com>
2024-04-01 21:09:08 +0530

    ChatApp

---
+++ src/main/java/com/tcs/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.tcs.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+
+	private String content;
+	private String sender;
+	private MessageType type;
+
+
+	public enum MessageType{
+		CHAT,LEAVE,JOIN
+	}
+
+}
